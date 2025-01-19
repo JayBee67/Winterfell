1f08e6a99543533c85029955ef3ae67e0231cce4
JayBee67 <jkd.benjamins@gmail.com>
2025-01-19 18:09:41 +0100

    Added hazard
    Added pendulum

---
+++ assets/scripts/hazards/pendulum/pendulum.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+var anim: Animation
+var max_rotation: float
+var min_rotation: float
+
+
+func _ready() -> void:
+	var anim_name = animation_player.get_animation("swing")
+	randomize()
+	min_rotation = randf_range(-40.0, -85.0)
+	max_rotation = abs(min_rotation)
+	anim_name.track_insert_key(0, 1.5, 1)
+	anim_name.track_set_key_value(0, 0, deg_to_rad(min_rotation))
+	anim_name.track_set_key_value(0, 1, deg_to_rad(max_rotation))
+	$AnimationPlayer.play("swing")
