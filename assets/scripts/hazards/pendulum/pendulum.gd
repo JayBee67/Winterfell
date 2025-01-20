5a3b546a3d23b62504264388df202a4f333236e5
JayBee67 <jkd.benjamins@gmail.com>
2025-01-20 22:18:43 +0100

    Update pendulum.gd

--- assets/scripts/hazards/pendulum/pendulum.gd
+++ assets/scripts/hazards/pendulum/pendulum.gd
@@ -1,7 +1,6 @@
 extends Node2D
 
-@onready var animation_player: AnimationPlayer = $AnimationPlayer
-@onready var anim_name = animation_player.get_animation("swing")
+@onready var anim_name = $AnimationPlayer.get_animation("swing")
 
 var min_rotation: float
 var max_rotation: float
@@ -9,10 +8,9 @@ var max_rotation: float
 
 func _ready() -> void:
 	randomize()
-	min_rotation = randf_range(-40.0, -85.0)
+	min_rotation = randf_range(-30.0, -85.0)
 	max_rotation = abs(min_rotation)
 	anim_name.track_insert_key(0, 0.0, 0)
 	anim_name.track_set_key_value(0, 0, deg_to_rad(min_rotation))
 	anim_name.track_insert_key(0, 1.5, 1)
 	anim_name.track_set_key_value(0, 1, deg_to_rad(max_rotation))
-	prints(min_rotation, max_rotation)
