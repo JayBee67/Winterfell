40e0a4a574d7a88a46e5bb8cc8bf5617c4f0fa24
JayBee67 <jkd.benjamins@gmail.com>
2025-01-20 19:57:24 +0100

    Added pendulum
    3 pendulums each starting from a unique rotation angle

--- assets/scripts/hazards/pendulum/pendulum.gd
+++ assets/scripts/hazards/pendulum/pendulum.gd
@@ -1,14 +1,13 @@
 extends Node2D
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var anim_name = animation_player.get_animation("swing")
 
-var anim: Animation
-var max_rotation: float
 var min_rotation: float
+var max_rotation: float
 
 
 func _ready() -> void:
-	var anim_name = animation_player.get_animation("swing")
 	randomize()
 	min_rotation = randf_range(-40.0, -85.0)
 	max_rotation = abs(min_rotation)
@@ -16,3 +15,4 @@ func _ready() -> void:
 	anim_name.track_set_key_value(0, 0, deg_to_rad(min_rotation))
 	anim_name.track_insert_key(0, 1.5, 1)
 	anim_name.track_set_key_value(0, 1, deg_to_rad(max_rotation))
+	prints(min_rotation, max_rotation)
